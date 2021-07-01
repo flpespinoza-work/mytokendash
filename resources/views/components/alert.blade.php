ec9d33ab5f2a652e774cc77d194f2a4fe51bfcdd
Felipe Espinoza <felipe.espinoza@tokencash.mx>
2021-07-01 16:19:40 -0500

    posicion de alert

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -2,7 +2,7 @@
     'title', 'message', 'type'
 ])
 
-<div class="absolute top-1 w-80 right-4 px-4 py-4 rounded-md bg-white border shadow-md sm:p-6 sm:pb-4 {{ $type }}">
+<div class="absolute top-1 w-11/12 sm:w-80 right-4 px-4 py-4 rounded-md bg-white border shadow-md sm:p-6 sm:pb-4 {{ $type }}">
         <div class="flex items-start">
             <div class="flex items-center justify-center flex-shrink-0 w-5 h-5 bg-red-100 rounded-full sm:h-7 sm:w-7">
                 <svg class="w-3 h-3 text-red-600 sm:h-5 sm:w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
