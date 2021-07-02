21b33db487ba0766601f5016c7b99c8526f9a2bd
Felipe Espinoza <felipe.espinoza@tokencash.mx>
2021-07-02 17:00:35 -0500

    Queries de reportes de cupones

--- app/Traits/Reports/Coupons.php
+++ app/Traits/Reports/Coupons.php
@@ -6,6 +6,19 @@ use Illuminate\Support\Facades\DB;
 trait Coupons
 {
 
+    function getRedemedCouponsDetail()
+    {
+
+    }
+
+    function getPrintedRedemedCoupons()
+    {
+        $printed = $this->getPrintedCoupons();
+        $redemed = $this->getRedemedCoupons();
+        $result = ['PRINTED' => $printed, 'REDEMED' => $redemed];
+        dd($result);
+    }
+
     function getRedemedCouponsHistory()
     {
         $extDb = DB::connection('tokencash');
@@ -15,8 +28,10 @@ trait Coupons
             $tmpRes = [];
             $extDb->table('dat_cupones')
             ->join('dat_cupones_canjeados', 'dat_cupones.CUP_ID', '=', 'dat_cupones_canjeados.CUP_CAN_CUPON')
+            ->join('cat_dbm_nodos_usuarios', 'dat_cupones_canjeados.CUP_CAN_NODO', '=', 'cat_dbm_nodos_usuarios.NOD_USU_NODO')
             ->select(DB::raw('COUNT(CUP_ID) CUPONES, SUM(CUP_CAN_MONTO) MONTO'))
             ->whereIn('CUP_GIFTCARD', ['SUPRA'])
+            ->whereRaw("(BINARY NOD_USU_CERTIFICADO REGEXP '[a-zA-Z0-9]+[o][CEFIKLNQSTWXYbcdgkmprsuvy24579]+[a-zA-Z0-9]+[o][CEFIKLNQSTWXYbcdgkmprsuvy24579]+[a-zA-Z0-9]+[o][CEFIKLNQSTWXYbcdgkmprsuvy24579]+[a-zA-Z0-9]' OR NOD_USU_CERTIFICADO = '')")
             ->groupBy('CUP_GIFTCARD')
             ->orderBy('MONTO', 'desc')
             ->chunk(10, function($coupons) use(&$tmpRes) {
@@ -32,11 +47,8 @@ trait Coupons
             return $tmpRes;
         });
 
-        dd($couponsArr);
-
         return $couponsArr;
     }
-
 
     function getRedemedCoupons()
     {
@@ -51,19 +63,16 @@ trait Coupons
             $extDb->table('dat_cupones')
             ->join('dat_cupones_canjeados', 'dat_cupones.CUP_ID', '=', 'dat_cupones_canjeados.CUP_CAN_CUPON')
             ->join('cat_dbm_nodos_usuarios', 'dat_cupones_canjeados.CUP_CAN_NODO', '=', 'cat_dbm_nodos_usuarios.NOD_USU_NODO')
-            ->select(DB::raw('DATE_FORMAT(CUP_CAN_FECHAHORA, "%Y/%m/%d") DIA, COUNT(CUP_ID) CUPONES, SUM(CUP_CAN_MONTO) MONTO, CUP_GIFTCARD GIFTCARD'))
+            ->select(DB::raw('DATE_FORMAT(CUP_CAN_FECHAHORA, "%Y/%m/%d") DIA, COUNT(CUP_ID) CUPONES, SUM(CUP_CAN_MONTO) MONTO'))
             ->whereIn('CUP_GIFTCARD', ['SUPRA'])
             ->whereBetween('CUP_CAN_FECHAHORA', [$initialDate, $finalDate])
-            ->whereRaw("BINARY NOD_USU_CERTIFICADO REGEXP '[a-zA-Z0-9]+[o][CEFIKLNQSTWXYbcdgkmprsuvy24579]+[a-zA-Z0-9]+[o][CEFIKLNQSTWXYbcdgkmprsuvy24579]+[a-zA-Z0-9]+[o][CEFIKLNQSTWXYbcdgkmprsuvy24579]+[a-zA-Z0-9]'")
-            ->orWhere('NOD_USU_CERTIFICADO', '=', '')
-            ->groupBy('DIA', 'GIFTCARD')
-            ->orderBy('GIFTCARD')
+            ->whereRaw("(BINARY NOD_USU_CERTIFICADO REGEXP '[a-zA-Z0-9]+[o][CEFIKLNQSTWXYbcdgkmprsuvy24579]+[a-zA-Z0-9]+[o][CEFIKLNQSTWXYbcdgkmprsuvy24579]+[a-zA-Z0-9]+[o][CEFIKLNQSTWXYbcdgkmprsuvy24579]+[a-zA-Z0-9]' OR NOD_USU_CERTIFICADO = '')")
+            ->groupBy('DIA', 'CUP_GIFTCARD')
             ->orderBy('DIA')
             ->chunk(10, function($coupons) use(&$tmpRes) {
                 foreach($coupons as $coupon)
                 {
                     $tmpRes[] = [
-                        'GIFTCARD' => $coupon->GIFTCARD,
                         'DIA' => $coupon->DIA,
                         'CUPONES' => $coupon->CUPONES,
                         'MONTO' => $coupon->MONTO
@@ -108,8 +117,8 @@ trait Coupons
     function getPrintedCoupons()
     {
         $extDb = DB::connection('tokencash');
-        $initialDate = '2021-06-01 00:00:00';
-        $finalDate = '2021-06-16 23:59:59';
+        $initialDate = '2021-07-01 00:00:00';
+        $finalDate = '2021-07-01 23:59:59';
         $reportId = md5($initialDate.$finalDate);
         $couponsArr = [];
 
@@ -118,7 +127,7 @@ trait Coupons
             $extDb->table('dat_cupones')
             ->join('dat_cupones_adicional', 'dat_cupones.CUP_ID', '=', 'dat_cupones_adicional.CUP_ADI_CUPON')
             ->select(DB::raw('DATE_FORMAT(CUP_TS, "%Y/%m/%d") DIA, COUNT(CUP_ID) CUPONES, SUM(CUP_ADI_AMOUNT) MONTO'))
-            ->whereIn('CUP_PRESUPUESTO', ['supra', 'isspx'])
+            ->whereIn('CUP_PRESUPUESTO', ['supra'])
             ->whereBetween('CUP_TS', [$initialDate, $finalDate])
             ->groupBy('DIA', 'CUP_PRESUPUESTO')
             ->orderBy('CUP_PRESUPUESTO')
