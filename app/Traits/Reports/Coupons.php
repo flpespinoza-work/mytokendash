b0bd8fdb55b4003542161c77aa17dabb6350f77b
Felipe Espinoza <felipe.espinoza@tokencash.mx>
2021-07-02 13:50:32 -0500

    cambio query cupones canjeados

--- app/Traits/Reports/Coupons.php
+++ app/Traits/Reports/Coupons.php
@@ -1,8 +1,6 @@
 <?php
 
 namespace App\Traits\Reports;
-
-use Exception;
 use Illuminate\Support\Facades\DB;
 
 trait Coupons
@@ -18,31 +16,40 @@ trait Coupons
 
         $couponsArr = cache()->remember('reporte-cupones-canjeados-' . $reportId, 60*1, function() use($extDb, $initialDate, $finalDate){
             $tmpRes = [];
-            $query = "SELECT DATE_FORMAT(CUP_CAN_FECHAHORA, \"%Y/%m/%d\") DIA,
-            COUNT(CUP_ID) CUPONES, SUM(CUP_CAN_MONTO) MONTO
-            FROM dat_cupones INNER JOIN dat_cupones_canjeados ON CUP_ID = CUP_CAN_CUPON
-            INNER JOIN cat_dbm_nodos_usuarios ON CUP_CAN_NODO = NOD_USU_NODO
-            WHERE (CUP_GIFTCARD = \"SUPRA\") AND (CUP_CAN_FECHAHORA >= \"{$initialDate}\" AND CUP_CAN_FECHAHORA <= \"{$finalDate}\")
-            AND ( ( BINARY substring(NOD_USU_CERTIFICADO, 11, 1) = 'o'
-            and BINARY substring(NOD_USU_CERTIFICADO, 12, 1) in ('C','E','F','I','K','L','N','Q','S','T','W','X','Y','b','c','d','g','k','m','p','r','s','u','v','y','2','4','5','7','9')
-            and BINARY substring(NOD_USU_CERTIFICADO, 63, 1) = 'o'
-            and BINARY substring(NOD_USU_CERTIFICADO, 64, 1) in ('C','E','F','I','K','L','N','Q','S','T','W','X','Y','b','c','d','g','k','m','p','r','s','u','v','y','2','4','5','7','9')
-            and BINARY substring(NOD_USU_CERTIFICADO,115, 1) = 'o'
-            and BINARY substring(NOD_USU_CERTIFICADO,116, 1) in ('C','E','F','I','K','L','N','Q','S','T','W','X','Y','b','c','d','g','k','m','p','r','s','u','v','y','2','4','5','7','9') )
-            OR ( NOD_USU_CERTIFICADO = \" \" ) )";
-
-            try
-            {
-                $extDb->table('dat_cupones')
-                ->select(DB::raw($query))
-                ->groupBy('DIA', 'CUP_GIFTCARD')
-                ->orderBy('DIA')
-                ->get();
-            }
-            catch(Exception $e)
-            {
-                print_r($e,true);
-            }
+            $extDb->table('dat_cupones')
+            ->join('dat_cupones_canjeados', 'dat_cupones.CUP_ID', '=', 'dat_cupones_canjeados.CUP_CAN_CUPON')
+            ->join('cat_dbm_nodos_usuarios', 'dat_cupones_canjeados.CUP_CAN_NODO', '=', 'cat_dbm_nodos_usuarios.NOD_USU_NODO')
+            ->select(DB::raw('DATE_FORMAT(CUP_CAN_FECHAHORA, "%Y/%m/%d") DIA, COUNT(CUP_ID) CUPONES, SUM(CUP_CAN_MONTO) MONTO, CUP_GIFTCARD GIFTCARD'))
+            ->whereIn('CUP_GIFTCARD', ['SUPRA'])
+            ->whereBetween('CUP_CAN_FECHAHORA', [$initialDate, $finalDate])
+            ->whereRaw(
+                "(
+                    BINARY substring(NOD_USU_CERTIFICADO, 11, 1) = 'o'
+                    and BINARY substring(NOD_USU_CERTIFICADO, 12, 1) in ('C','E','F','I','K','L','N','Q','S','T','W','X','Y','b','c','d','g','k','m','p','r','s','u','v','y','2','4','5','7','9')
+                    and BINARY substring(NOD_USU_CERTIFICADO, 63, 1) = 'o'
+                    and BINARY substring(NOD_USU_CERTIFICADO, 64, 1) in ('C','E','F','I','K','L','N','Q','S','T','W','X','Y','b','c','d','g','k','m','p','r','s','u','v','y','2','4','5','7','9')
+                    and BINARY substring(NOD_USU_CERTIFICADO,115, 1) = 'o'
+                    and BINARY substring(NOD_USU_CERTIFICADO,116, 1) in ('C','E','F','I','K','L','N','Q','S','T','W','X','Y','b','c','d','g','k','m','p','r','s','u','v','y','2','4','5','7','9')
+                )
+                OR
+                (
+                    NOD_USU_CERTIFICADO = \" \"
+                )"
+            )
+            ->groupBy('DIA', 'GIFTCARD')
+            ->orderBy('GIFTCARD')
+            ->orderBy('DIA')
+            ->chunk(10, function($coupons) use(&$tmpRes) {
+                foreach($coupons as $coupon)
+                {
+                    $tmpRes[] = [
+                        'GIFTCARD' => $coupon->GIFTCARD,
+                        'DIA' => $coupon->DIA,
+                        'CUPONES' => $coupon->CUPONES,
+                        'MONTO' => $coupon->MONTO
+                    ];
+                }
+            });
 
             return $tmpRes;
         });
