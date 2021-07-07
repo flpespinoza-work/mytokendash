76b6a27c338449328ed1ca594f892efa282b3f13
Felipe Espinoza <flpespinoza@gmail.com>
2021-07-07 00:04:35 -0500

    Agregar helpers

---
+++ app/Helpers.php
@@ -0,0 +1,16 @@
+<?php
+
+//Obtener establecimientos desde dbm tokencash
+
+//Obtener establecimientos registrados en el dashboard
+
+//Obtener usuarios segun el rol del usuario logueado
+
+//Obtener presupuestos
+
+//Obtener giftcards
+
+//Generar identificador para los reportes
+
+//Generar la cantidad de minutos que un reporte estara en la cache
+
